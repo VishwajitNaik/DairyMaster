0b5ec5e6e0487c331f7311a726508a610340c691
Vishwajit Naik <44186372+VishwajitNaik@users.noreply.github.com>
2024-11-13 18:03:42 +0530

    debugging log are removed

--- src/app/components/SessionMilk.js
+++ src/app/components/SessionMilk.js
@@ -29,6 +29,8 @@ export default function TodayMilkRecords() {
           const buffaloRecords = response.data.milkRecords.filter(record => record.milk === "म्हैस ");
           const cowRecords = response.data.milkRecords.filter(record => record.milk === "गाय ");
           const session = response.data.milkRecords[0].session;
+          console.log("Session:", session);
+          
           setSession(session);
           setMilkRecords(buffaloRecords);
           setCowMilkRecords(cowRecords);
